d5e8b87b4b92ae97d4a2d406873b5e0f5e80fb9c
PhanVanManh <imseirousmanh@gmail.com>
2026-04-06 22:25:45 +0700

    feat: add security configuration and API versioning support

---
+++ core/src/main/java/com/osamuharu/core/security/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.osamuharu.core.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RequestMapping.class));
+    }
+
+    @Override
+    public void configureApiVersioning(ApiVersionConfigurer configurer) {
+        configurer.usePathSegment(1)
+                .addSupportedVersions("v1")
+                .setDefaultVersion("v1");
+    }
+}
